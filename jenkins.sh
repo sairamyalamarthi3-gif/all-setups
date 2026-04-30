da5adbaeeea726dd4e22ae39ff3b3174685fbd70
Sairam Yalamarthi <sairamyalamarthi3@gmail.com>
2026-04-30 18:28:30 +0100

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,4 +1,4 @@
-sudo yum install java-17-amazon-corretto -y
+sudo yum install java-21-amazon-corretto -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/rpm-stable/jenkins.repo
 sudo yum upgrade
