53c0947fff0ab6c43ceb0ccf11f1183700636628
Sairam Yalamarthi <sairamyalamarthi3@gmail.com>
2026-05-02 11:32:53 +0100

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -1,17 +1,30 @@
-#vim .bashrc
-#export PATH=$PATH:/usr/local/bin/
-#source .bashrc
-
-#! /bin/bash
+## Download kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
-chmod +x kops
-sudo mv kops /usr/local/bin/kops
 chmod +x kubectl
-mv kubectl /usr/local/bin/kubectl
+sudo mv kubectl /usr/local/bin/kubectl
 
-aws s3api create-bucket --bucket mustafa.project.flm.k8s.local --region us-east-1
-aws s3api put-bucket-versioning --bucket mustafa.project.flm.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
-export KOPS_STATE_STORE=s3://mustafa.project.flm.k8s.local
-kops create cluster --name mustafa.k8s.local --zones us-east-1a,us-east-1b --master-count=1 --master-size t2.large --master-volume-size=30 --node-count=3 --node-size t2.medium --node-volume-size=20
-kops update cluster --name mustafa.k8s.local --yes --admin
+## Install Kops
+wget https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+chmod +x kops-linux-amd64
+sudo mv kops-linux-amd64 /usr/local/bin/kops
+
+## S3 Bucket Setup
+aws s3api create-bucket --bucket sair.project.flm.k8s.local --region us-east-1
+aws s3api put-bucket-versioning --bucket sair.project.flm.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
+
+## Export State Store
+export KOPS_STATE_STORE=s3://sair.project.flm.k8s.local
+
+## Create Cluster
+kops create cluster \
+  --name sairam.k8s.local \
+  --zones us-east-1a,us-east-1b \
+  --control-plane-count 1 \
+  --control-plane-size c7i-flex.large \
+  --control-plane-volume-size 30 \
+  --node-count 3 \
+  --node-size t2.medium \
+  --node-volume-size 20
+
+## Update Cluster
+kops update cluster --name sairam.k8s.local --yes --admin
