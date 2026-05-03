bafd70183f6f194af82c912743c40b3c99243af4
Sairam Yalamarthi <sairamyalamarthi3@gmail.com>
2026-05-03 12:45:38 +0100

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -9,22 +9,22 @@ chmod +x kops-linux-amd64
 sudo mv kops-linux-amd64 /usr/local/bin/kops
 
 ## S3 Bucket Setup
-aws s3api create-bucket --bucket sair.project.flm.k8s.local --region us-east-1
-aws s3api put-bucket-versioning --bucket sair.project.flm.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
+aws s3api create-bucket --bucket ram.project.flm.k8s.local --region us-east-1
+aws s3api put-bucket-versioning --bucket ram.project.flm.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
 
 ## Export State Store
-export KOPS_STATE_STORE=s3://sair.project.flm.k8s.local
+export KOPS_STATE_STORE=s3://ram.project.flm.k8s.local
 
 ## Create Cluster
 kops create cluster \
-  --name sairam.k8s.local \
+  --name ram.k8s.local \
   --zones us-east-1a,us-east-1b \
   --control-plane-count 1 \
   --control-plane-size c7i-flex.large \
   --control-plane-volume-size 30 \
   --node-count 3 \
-  --node-size t2.medium \
+  --node-size t3.micro \
   --node-volume-size 20
 
 ## Update Cluster
-kops update cluster --name sairam.k8s.local --yes --admin
+kops update cluster --name ram.k8s.local --yes --admin
